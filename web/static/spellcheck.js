7e1f19a8675e5074c4be71e0911583bb61ece308
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-08-06 22:02:27 +0530

    Spellcheck: Add spell correction feature from suggestion

--- web/static/spellcheck.js
+++ web/static/spellcheck.js
@@ -3,6 +3,7 @@ function doSpellcheck() {
 	let editor = document.querySelector('.sp-editor');
 	prepare(editor);
 	process();
+
 }
 
 function prepare(editor) {
@@ -10,15 +11,24 @@ function prepare(editor) {
 }
 
 function checkWord(word) {
+	if (resultDictionary[word] && resultDictionary[word].result) {
+		return new Promise((resolve, reject) => { resolve("Value already checked"); });
+	}
 	return $.getJSON('/api/spellcheck', { word }, (data) => {
 		resultDictionary[word].result = data;
 	});
 }
 
-function addMenuItem(label) {
+function addMenuItem(label, word) {
 	var menuitem = document.createElement("menuitem");
 	menuitem.label = label;
+	menuitem.for = word;
 	document.querySelector("#suggestions").appendChild(menuitem);
+	menuitem.addEventListener("click", (event) => {
+		let word = event.target.for;
+		resultDictionary[word].node.innerText = event.target.label;
+		resultDictionary[word].node.classList.remove("error");
+	});
 }
 
 function process() {
@@ -26,27 +36,31 @@ function process() {
 	let i = words.length;
 	while (i--) {
 		let word = words[i].innerText;
-		resultDictionary[word] = { node: words[i] };
-		checkWord(word).then(() => {
-			if (resultDictionary[word].result.correct) {
-				resultDictionary[word].node.classList.remove("error");
-				return false;
-			}
-			resultDictionary[word].node.classList.add("error");
-			resultDictionary[word].node.addEventListener(
-				"contextmenu",
-				onContextClick,
-				false
-			);
-		});
+		if (!resultDictionary[word]) {
+			resultDictionary[word] = {};
+		}
+		resultDictionary[word].node = words[i];
+		resultDictionary[word].node.classList.remove("error");
+		checkWord(word).then(() => onResult(word) );
 	}
 }
 
+function onResult(word) {
+	if (resultDictionary[word].result.correct) {
+		return false;
+	}
+	resultDictionary[word].node.classList.add("error");
+	resultDictionary[word].node.addEventListener(
+		"contextmenu",
+		onContextClick,
+		false
+	);
+}
 function onContextClick() {
 	document.querySelector("#suggestions").innerHTML = "";
 	let word = this.innerText;
 	let suggestions = resultDictionary[word].result.suggestions;
 	for (let i = 0; i < suggestions.length; i++) {
-		addMenuItem(suggestions[i]);
+		addMenuItem(suggestions[i], word);
 	}
 }
