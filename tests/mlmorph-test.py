e3ed6dce4fecb506656f40f3edf1028e7fd180ff
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-11-12 20:05:36 +0530

    Increase coverage test corpora

--- tests/mlmorph-test.py
+++ tests/mlmorph-test.py
@@ -73,7 +73,7 @@ class AnalyserGeneratorTests(unittest.TestCase):
         tokens_count = 0
         analysed_tokens_count = 0
         missed_words = []
-        for file_number in range(1,19):
+        for file_number in range(1,24):
             file = 'coverage/{0:d}.txt'.format(file_number)
             with open(file, 'r') as f:
                 for line in f:
