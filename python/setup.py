a4b6d08480cb228ddae7bc7991400661280399c3
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2018-12-25 10:05:16 +0530

    Update version

--- python/setup.py
+++ python/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     zip_safe=False,
+    version=open('VERSION').read().strip(),
     entry_points={
         'console_scripts': [
             'mlmorph = mlmorph.__main__:main'
