b153190d1d97fe160210dc2dbda71ee19cb04592
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2020-06-26 18:02:28 +0530

    use mlmorph instead of hfst-lookup

--- scripts/print_unsupported_words.sh
+++ scripts/print_unsupported_words.sh
@@ -1,1 +1,1 @@
-sed 's/\s/\n/g'| sed  -f ./scripts/corpora-cleanup.sed | hfst-lookup malayalam.analyser | awk '/inf$/ {print $1}' | sort -u
+sed 's/\s/\n/g'| sed  -f ./scripts/corpora-cleanup.sed | mlmorph -a| awk '/?$/ {print $1}' | sort -u
