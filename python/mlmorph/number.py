70630a54e1ed8083c5e9400d362fa8456dc9d9dd
home <krishna@dsplog.com>
2021-12-01 05:43:43 +0530

    fixing brackets

--- python/mlmorph/number.py
+++ python/mlmorph/number.py
@@ -11,15 +11,15 @@ onesStr = [
   "ഒമ്പത്"
 ]
 
+
 def clean(result) : 
       result = result.replace("<ones><hundreds>", "<hundreds>")
       result = result.replace("<ones><tens>", "<tens>")
-      result = result.replace("ഒന്ന്<ones><hundredsa>", "<hundreds>")
+      result = result.replace("ഒന്ന്<ones><hundreds>", "<hundreds>")
       result = result.replace("ഒന്ന്<ones><thousands>", "<thousands>")
       return result
 
-
-def positionValues(value) : 
+def positionValues(value) :
       result = ""
       crores = int(value / 10000000) if (value >= 10000000) else 0
       lakhs = int((value % 10000000) / 100000)
@@ -27,13 +27,13 @@ def positionValues(value) :
       hundreds = int((value % 1000) / 100)
       tens = int((value % 100) / 10)
       ones = int((value % 10) / 1)
-      result =  (positionValues(crores) + "<crores>" if (crores > 0) else "" +
-        (positionValues(lakhs) + "<lakhs>" if (lakhs > 0)  else "") +
-        (positionValues(thousands) + "<thousands>" if (thousands > 0) else "") +
-        (positionValues(hundreds) + "<hundreds>" if (hundreds > 0) else "") +
-        (positionValues(tens) + "<tens>" if (tens > 0) else "") +
-        (onesStr[ones] + "<ones>" if (ones > 0) else "") +
-        (onesStr[ones] + "<zero>" if (value == 0) else ""))
+      result = ((positionValues(crores) + "<crores>") if (crores > 0) else  "") + \
+        ((positionValues(lakhs) + "<lakhs>") if (lakhs > 0) else "") + \
+        ((positionValues(thousands) + "<thousands>") if (thousands > 0) else "") + \
+        ((positionValues(hundreds) + "<hundreds>") if (hundreds > 0) else "") + \
+        ((positionValues(tens) + "<tens>") if (tens > 0) else "") + \
+        ((onesStr[ones] + "<ones>") if (ones > 0) else "") + \
+        ((onesStr[ones] + "<zero>") if (value == 0) else "")
       return clean(result)
 
 def spellOut(value) : 
