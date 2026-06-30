f055513ef5098c4ea488f0b07095d77eb494ddd7
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2026-06-30 14:36:18 +0530

    fix(__init__): replace function objects with quoted strings in __all__

--- python/mlmorph/__init__.py
+++ python/mlmorph/__init__.py
@@ -1,7 +1,6 @@
-
 from .analyser import Analyser, normalize
 from .foreign_word_detector import check_foreign_word
 from .generator import Generator
 from .utils import tokenize
 
-__all__ = ['Generator', 'Analyser', 'check_foreign_word', tokenize, normalize]
+__all__ = ["Generator", "Analyser", "check_foreign_word", "tokenize", "normalize"]
